85cfe858daf116cace8f91028618b3223403bd5a
anthonyshikanga <anthonyshikanga@gmail.com>
2017-08-01 23:11:16 +0300

    added flipping

--- js/main.js
+++ js/main.js
@@ -18,7 +18,16 @@ var GameState = {
    this.background = this.game.add.sprite(0, 0, 'background');
 
    this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
-   this.chicken.anchor.setTo(0.5);
+   this.chicken.anchor.setTo(0.5, 0.5);
+   this.chicken.scale.setTo(2, 1);
+
+   this.horse = this.game.add.sprite(120, 10, 'horse');
+   this.horse.scale.setTo(0.5);
+
+   this.pig = this.game.add.sprite(500, 300, 'pig');
+   this.pig.anchor.setTo(0.5);
+   this.pig.scale.setTo(-1, 1);
+
 
   },
   update: function() {
