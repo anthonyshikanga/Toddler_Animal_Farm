bff67d03a6ba46e394e1272e1b20877ab265c118
anthonyshikanga <anthonyshikanga@gmail.com>
2017-08-01 23:01:54 +0300

    added scaling

--- js/main.js
+++ js/main.js
@@ -9,6 +9,12 @@ var GameState = {
    this.load.image('sheep','assets/images/');
   },
   create: function() {
+   this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+   this.scale.pageAlignHorizontally = true;
+   this.scale.pageAlignVertically = true;
+
+
    this.background = this.game.add.sprite(0, 0, 'background');
 
    this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
