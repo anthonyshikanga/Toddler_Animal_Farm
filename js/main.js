c6603be2fa52dc81ecca7385e11f8340cd312e3d
anthonyshikanga <anthonyshikanga@gmail.com>
2017-08-01 20:02:07 +0300

    added sprite anchor points

--- js/main.js
+++ js/main.js
@@ -3,9 +3,17 @@ var game = new Phaser.Game(640, 360, Phaser.AUTO);
 var GameState = {
   preload: function(){
    this.load.image('background', 'assets/images/');
+   this.load.image('chicken', 'assets/images/');
+   this.load.image('horse', 'assets/images/');
+   this.load.image('pig', 'assets/images/');
+   this.load.image('sheep','assets/images/');
   },
   create: function() {
    this.background = this.game.add.sprite(0, 0, 'background');
+
+   this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
+   this.chicken.anchor.setTo(0.5);
+
   },
   update: function() {
 
